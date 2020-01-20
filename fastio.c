9cf3f10c12a656f59bb78b8631d7fa6a924c3b55
Michael Chelli <michaelchelli00@gmail.com>
2020-01-20 17:14:49 +0100

    aaaaaaa

--- fastio.c
+++ fastio.c
@@ -1,8 +1,8 @@
-//#include <unistd.h>
-#include <stdio.h>
+#include <unistd.h>
+//#include <stdio.h>
 
-#define NBIN 100000
-#define NBOUT 100000
+#define NBIN 8000000
+#define NBOUT 20
 
 unsigned char input[NBIN];
 unsigned inputi;
@@ -10,65 +10,69 @@ unsigned inputi;
 unsigned char output[NBOUT];
 unsigned outputi;
 
-#define get_unsigned(x){\
+//if values are already 0, you can delete first line
+#define get_uns(x){\
 	x=0;\
 	while(input[inputi]<=32)inputi++;\
-	while(input[inputi]>='0')x=x*10+input[inputi++]-'0';\
+	while(input[inputi]>32)x=x*10+input[inputi++]-'0';\
 }
+
 #define get_int(x){\
 	x=0;\
-	/*bool*/unsigned char sign=0;\
+	unsigned char sign=0;\
 	while(input[inputi]<=32)inputi++;\
 	if(input[inputi]=='-'){sign=1;inputi++;}\
 	while(input[inputi]>='0')x=x*10+input[inputi++]-'0';\
 	if(sign)x=-x;\
 }
+
 #define get_char(x){\
 	x=input[inputi++];\
 }
 
+
+//can't print 0, modifies value (thus x has to be lvalue)
 #define put_uns(x){\
-	unsigned char b[22];\
-	int t=0;\
+	unsigned char _put_b[22];\
+	char _put_t=0;\
 	while(x){\
-		b[t++]=x%10+'0';\
+		_put_b[_put_t++]=x%10+'0';\
 		x/=10;\
 	}\
-	while(--t>=0)output[outputi++]=b[t];\
+	while(--_put_t>=0)output[outputi++]=_put_b[_put_t];\
 }
+
 #define put_int(x){\
-	unsigned char b[22];\
-	int t=0;\
 	if(x<0){\
-		x=-x;\
 		output[outputi++]='-';\
+		x=-x\
 	}\
-	while(x){\
-		b[t++]=x%10+'0';\
-		x/=10;\
-	}\
-	while(--t>=0)output[outputi++]=b[t];\
+	put_uns(x)\
 }
+
 #define put_char(x){\
 	output[outputi++]=x;\
 }
+
 void put_str(char *x){
 	while(*x)output[outputi++]=*(x++);
 }
 
-
 int main(){
-	//read(0,input,NBIN);
-	fread_unlocked(input,1,NBIN,stdin);
+	read(0,input,NBIN);
+	//fread_unlocked(input,1,NBIN,stdin);
 
-	int x;
-	get_int(x);
-	put_int(x);
-	put_char('x');
-	put_char('\n');
-	put_str("YES\n");
+	unsigned x;
+	get_uns(x);
+	if(x==0)put_str("x=0\n");
+	else {
+		put_str("x=");
+		put_uns(x);
+		put_char('\n');
+	}
 
-	//write(1,output,outputi);
-	fwrite_unlocked(output,1,outputi,stdout);
+	write(1,output,outputi);
+	//fwrite_unlocked(output,1,outputi,stdout);
+	return 0;
 }
 
