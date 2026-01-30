03c0d6f500d952126b503cd019ed1428849552a6
Michael Chelli <michaelchelli00@gmail.com>
2026-01-30 15:39:12 +0100

    safer formatting script

--- format.sh
+++ format.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
 clang-format -i $1
-sed -i 's/ \([*+-/%&|^><]\|>>\|<<\|>=\|<=\|==\|!=\) /\1/g' $1
+V1=$(sed 's/ \([*+-/%&|^><]\|>>\|<<\|>=\|<=\|==\|!=\) /\1/g' "$1" | clang-format)
+V2=$(cat "$1")
+if [ "$V1" = "$V2" ]; then
+	sed -i 's/ \([*+-/%&|^><]\|>>\|<<\|>=\|<=\|==\|!=\) /\1/g' $1
+else
+	echo "Could not sed because it does not preserve the code :("
+	echo "WRONG:"
+	echo "$V1"
+	echo "CORRECT:"
+	echo "$V2"
+fi;
 
